f989d2c615c4086e9e377c0f769ebcc6a9a5bc7b
Dylan <deamaral.dylan@gmail.com>
2019-03-10 19:56:27 -0400

    wip

--- dm_bot/Commands/JobCommand.cs
+++ dm_bot/Commands/JobCommand.cs
@@ -21,14 +21,38 @@ namespace dm_bot.Commands {
         [Command ("jobs")]
         public async Task AddJobAsync ([Remainder] string message = null) {
             try {
-                var context = Context.User;
+                var user = Context.User;
 
+                var token = message.Split (" ", 2);
+                if (token.Length < 2) {
+                    await ReplyAsync ($"you must specify a command, use '!dm jobs help' for help.");
+                    return;
+                }
                 var job = new Job ();
+
+                var operation = token[0].ToLower ();
+
+                switch (operation) {
+                    case "update":
+                        ParseJobUpdateRequest (message);
+                    case "find":
+
+                    case "list":
+
+                        break;
+                    default:
+                        break;
+                }
 
                 await ReplyAsync ($"Added new job, '{job.Title}', to update the description, use `!dm jobs update {job.Id} \"Your description here\"`");
             } catch (ParseException exception) {
                 await ReplyAsync ($"Sorry {message}, we cannot process that request.");
             }
         }
+
+        private bool ParseJobUpdateRequest () {
+
+        }
+
     }
 }
