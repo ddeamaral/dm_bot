7ad58a8dd37c02d2a1e4dc6e7b2ff9564ee7595c
Dylan <deamaral.dylan@gmail.com>
2019-03-11 08:21:04 -0400

    adds ability to add a new job, and list existing ones

--- dm_bot/Commands/JobCommand.cs
+++ dm_bot/Commands/JobCommand.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using dm_bot.Contexts;
 using dm_bot.Extensions;
@@ -24,33 +25,87 @@ namespace dm_bot.Commands {
                 var user = Context.User;
 
                 var token = message.Split (" ", 2);
-                if (token.Length < 2) {
+                if (token.Length < 2 && !(token.Length == 1 && token[0].ToLower () == "list")) {
                     await ReplyAsync ($"you must specify a command, use '!dm jobs help' for help.");
                     return;
                 }
                 var job = new Job ();
 
                 var operation = token[0].ToLower ();
+                string m = "";
+
+                if (operation != "list") {
+                    m = message.Substring (message.IndexOf (" ")).Trim ();
+                }
 
                 switch (operation) {
-                    case "update":
-                        ParseJobUpdateRequest (message);
-                    case "find":
-
-                    case "list":
-
+                    case "add":
+                        await AddNewJob (m);
                         break;
+                    case "update":
+                        ParseJobUpdateRequest (m);
+                        break;
+                    case "find":
+                        await SearchExistingJobs (m);
+                        break;
+                    case "list":
+                        await ListJobs ();
+                        break;
+                    case "help":
+                        ShowHelpMessage ();
+                        break;
+
                     default:
                         break;
                 }
 
-                await ReplyAsync ($"Added new job, '{job.Title}', to update the description, use `!dm jobs update {job.Id} \"Your description here\"`");
             } catch (ParseException exception) {
                 await ReplyAsync ($"Sorry {message}, we cannot process that request.");
             }
         }
 
-        private bool ParseJobUpdateRequest () {
+        private async Task AddNewJob (string message) {
+            var job = new Job () { Title = message };
+
+            _db.Jobs.Add (job);
+            await _db.SaveChangesAsync ();
+            await ReplyAsync ($"Added new job, '{job.Title}', to update the description, use `!dm jobs update {job.Id} \"Your description here\"`");
+        }
+
+        private void ShowHelpMessage () {
+            throw new NotImplementedException ();
+        }
+
+        private async Task SearchExistingJobs (string message) {
+            var matches = _db.Jobs.Where (job => job.Title.ToLower ().Contains (message.ToLower ())).ToList ();
+
+            if (matches.Count > 0) {
+                await SendJobs (matches);
+                return;
+            }
+
+            await ReplyAsync ($"Could not find a job with that title, please double check it exists");
+        }
+
+        private async Task ListJobs () {
+            var jobs = _db.Jobs.ToList ();
+
+            await SendJobs (jobs);
+        }
+
+        private async Task SendJobs (List<Job> jobs) {
+            var sb = new StringBuilder ();
+            for (int i = 0; i < jobs.Count; i++) {
+
+                sb.AppendLine ($"{jobs[i].Id}) {jobs[i].Title} Difficulty: {jobs[i].Difficulty}");
+            }
+
+            var embedBuilder = new EmbedBuilder ();
+            embedBuilder.WithDescription (sb.ToString ());
+            await Context.Channel.SendMessageAsync ("", false, embedBuilder.Build ());
+        }
+
+        private void ParseJobUpdateRequest (string message) {
 
         }
 
